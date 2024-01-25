ccc5982ef2328cf60fc67e45fd4caa4e1e4c3dbf
Ilham Natsir <ilhamnatsirb@gmail.com>
2024-01-25 20:47:13 +0800

    update notifikasi

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import wiringpi
 import time
 from wiringpi import GPIO
 import os  # Tambahkan ini untuk mengimpor modul os
+from pyfcm import FCMNotification
+
 
 # Inisialisasi WiringPi
 wiringpi.wiringPiSetup()
@@ -15,6 +17,21 @@ led_pin = 6
 wiringpi.pinMode(pir_pin, GPIO.INPUT)
 wiringpi.pinMode(led_pin, GPIO.OUTPUT)
 
+
+# Ganti dengan kunci server Firebase Anda
+push_service = FCMNotification(api_key="AIzaSyBKoixbI0LyS93C1mQ2ZA_Slz3BEXN9Xqw")
+
+# Token perangkat Android
+registration_id = "fcp4YhjjSEmJQjh2nuVwNS:APA91bEOZoquLFRGEfPhs0IWT3YU4xlONepWo-q4EQlvpLlwEQw82t4TmgUj-Upz1gBzDegkb8RKm_7ml9sJV5xsyma3QbXbZyc8nIz3hDbKIk1J32T5et0EBAPFsmRGh19lJkZS6p66"
+
+
+# @app.route('/pushnotif')
+def pushnotif():
+    # Kirim pesan
+    message_title = "Peringatan!"
+    message_body = "Gerakan Terdeteksi"
+    result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+    print(result)
 
 def capture_image():
     # Inisialisasi objek VideoCapture dengan indeks kamera. Biasanya, kamera internal memiliki indeks 0.
@@ -65,6 +82,7 @@ try:
         if (pir_value == 1):
             wiringpi.digitalWrite(led_pin, GPIO.HIGH)
             capture_image()
+            pushnotif()
 
         else:
             wiringpi.digitalWrite(led_pin, GPIO.LOW)
