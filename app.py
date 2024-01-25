728afbeb4f2906a7919e00ee8213eb4a9dc8c49b
Ilham Natsir <ilhamnatsirb@gmail.com>
2024-01-25 23:35:53 +0800

    update credential

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from pyfcm import FCMNotification
 import firebase_admin
 from firebase_admin import credentials, messaging
 
+cred = credentials.Certificate("google-services.json")
+firebase_admin.initialize_app(cred)
 tokens = "eGvKDO6wRieOXj_TlOF7ub:APA91bHmyDIcaC_UPXV21rjEFSzoq19OY3aB473ebDh8ORxb6gy5dCFCSCh8qqf8YqzhWxSFA2fsv2t2lIlBoLY2ckVQ-ey0CYDhGc9vkssy7NuRHkeEfdxnTSsT4sS6LSv8_NEcmzPY"
 
 
